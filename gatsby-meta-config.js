924b1eeaf1754e41ca65175da57cba1804e4245c
CoodingPenguin <unodostre@naver.com>
2020-03-31 02:26:19 +0900

    Change width and metadata

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,27 +1,28 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  title: `코딩하는펭귄의 저장소`,
+  description: ``,
+  author: `코딩하는펭귄`,
+  introduction: `건강한 개발자(Healthy Developer)를 꿈꾸고 있습니다🏃‍♀️`,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `CoodingPenguin`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
+    instagram: `cooding_penguin`,
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'CoodingPenguin/devlog', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: '',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
