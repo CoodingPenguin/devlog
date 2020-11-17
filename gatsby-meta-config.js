d4153214eb0e595dbf000d1e939cf2c53db34d0a
CoodingPenguin <unodostre@naver.com>
2020-11-17 21:14:07 +0900

    Test ifbbio having hashtag didn't work

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: `코딩하는펭귄의 저장소`,
   description: ``,
   author: `코딩하는펭귄`,
-  introduction: `#React #Django #Python #개발자`,
+  introduction: `TEST`,
   siteUrl: `https://cooding-penguin.netlify.app`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
