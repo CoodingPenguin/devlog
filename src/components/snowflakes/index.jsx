1142e265dba83d27b057d8f6f6e1eb2b4c1d3d63
CoodingPenguin <unodostre@naver.com>
2020-12-06 18:07:24 +0900

    refactor: 💡 add snowing effect

---
+++ src/components/snowflakes/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import './index.scss'
+
+export const Snowflakes = () => (
+  <div class="snowflakes" aria-hidden="true">
+    <div class="snowflake">❅</div>
+    <div class="snowflake">❆</div>
+    <div class="snowflake">❅</div>
+    <div class="snowflake">❆</div>
+    <div class="snowflake">❅</div>
+    <div class="snowflake">❆</div>
+    <div class="snowflake">❅</div>
+    <div class="snowflake">❆</div>
+    <div class="snowflake">❅</div>
+    <div class="snowflake">❆</div>
+    <div class="snowflake">❅</div>
+    <div class="snowflake">❆</div>
+    <div class="snowflake">❆</div>
+    <div class="snowflake">❅</div>
+    <div class="snowflake">❆</div>
+    <div class="snowflake">❅</div>
+    <div class="snowflake">❆</div>
+  </div>
+)
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -4,6 +4,7 @@ import { Top } from '../components/top'
 import { Header } from '../components/header'
 import { ThemeSwitch } from '../components/theme-switch'
 import { Footer } from '../components/footer'
+import { Snowflakes } from '../components/snowflakes'
 import { rhythm } from '../utils/typography'
 
 import './index.scss'
@@ -22,6 +23,7 @@ export const Layout = ({ location, title, children }) => {
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
+        <Snowflakes />
         <ThemeSwitch />
         <Header title={title} location={location} rootPath={rootPath} />
         {children}
