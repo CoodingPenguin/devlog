2cd1bb995392ffaf73f2888a2a6f3215023f275b
CoodingPenguin <coodingpenguin@gmail.com>
2024-02-25 17:29:21 +0900

    Change home page title

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,5 +1,5 @@
 const headerNavLinks = [
-  { href: '/', title: 'Home' },
+  { href: '/', title: '홈' },
   { href: '/blog', title: '블로그' },
   { href: '/tags', title: '태그' },
   { href: '/about', title: '소개' },
