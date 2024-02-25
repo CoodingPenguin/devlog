0b21d1579960cb2eff14bf5ac37e52028fdd4e7a
CoodingPenguin <coodingpenguin@gmail.com>
2024-02-25 13:54:41 +0900

    Downgrade nextjs to 14.0.4 to make giscus work

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -5,10 +5,10 @@ import { useState } from 'react'
 import siteMetadata from '@/data/siteMetadata'
 
 export default function Comments({ slug }: { slug: string }) {
-  const [loadComments, setLoadComments] = useState(false)
+  const [loadComments, setLoadComments] = useState(true)
   return (
     <>
-      {!loadComments && <button onClick={() => setLoadComments(true)}>댓글 보기</button>}
+      {/*{!loadComments && <button onClick={() => setLoadComments(true)}>댓글 보기</button>}*/}
       {siteMetadata.comments && loadComments && (
         <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
       )}
