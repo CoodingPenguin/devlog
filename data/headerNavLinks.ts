27fff595f4439ec7be5e5044c38b9a407eb7a305
CoodingPenguin <coodingpenguin@gmail.com>
2024-02-28 00:39:02 +0900

    Add project pages

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/', title: '홈' },
   { href: '/blog', title: '블로그' },
   { href: '/tags', title: '태그' },
+  { href: '/projects', title: '프로젝트' },
   { href: '/about', title: '소개' },
 ]
 
