30df1f900cdd951eccb4b3da74fd873d53d444d9
CoodingPenguin <coodingpenguin@gmail.com>
2024-03-10 16:12:29 +0900

    Remove series link

--- layouts/SeriesListLayout.tsx
+++ layouts/SeriesListLayout.tsx
@@ -4,12 +4,11 @@ import Link from '@/components/Link'
 import Tag from '@/components/Tag'
 
 export default function SeriesListLayount({ posts, title }) {
-  console.log(posts)
   return (
     <div className="divide-y divide-gray-200 dark:divide-gray-700">
       <div className="space-y-2 pb-8 pt-6 md:space-y-5">
         <h1 className="font-calli text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-          <Link href="/series">{title}</Link>
+          {title}
         </h1>
       </div>
       <ul className="divide-y divide-gray-200 dark:divide-gray-700">
